b60a578601e438d66f87678cfea3271f37c1b844
pyk4239-debug <pyk4239@gmail.com>
2026-04-09 16:31:30 +0900

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,13 @@
-const CACHE = "newspaper-v1";
-const ASSETS = [
-  "/",
-  "/index.html",
-];
+const CACHE = "newspaper-v2";
 
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
@@ -22,6 +15,7 @@ self.addEventListener("activate", e => {
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match("/")))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
+Commit changes 하면 끝이에요!
